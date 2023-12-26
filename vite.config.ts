e09513725a55f50bd924d57c2f2f96a9705b0df6
eliasyoung <elias.y.warlock@gmail.com>
2023-12-26 12:34:09 +0800

    some configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,17 @@ import { defineConfig } from "vite";
 
 // module.exports = defineConfig({});
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/extension.ts",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vscode"],
+    },
+    emptyOutDir: false,
+    outDir: "dist/compiled",
+  },
+});
